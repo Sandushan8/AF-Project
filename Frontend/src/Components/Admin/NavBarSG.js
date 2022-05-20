df164b44c6fbee3e07663127520861c8435b82c3
Ihill Dewpura <88815545+Sandushan8@users.noreply.github.com>
2022-05-20 16:10:44 +0530

    Templates

--- Frontend/src/Components/Admin/NavBarSG.js
+++ Frontend/src/Components/Admin/NavBarSG.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import '../../css/Admin/NavBarA.css'
-export const NavBarA = () => {
+export const NavBarSG = () => {
   return (
     <div className='navA'>
         <nav className='Adnav'>
-            <label className='alogo'>ADMIN</label>
+            <label className='alogo'>STUDENT GROUPS</label>
             <ul className='a_navul'>
             <Link to='/AdminHome'><li className='a_navli'>Home</li></Link>
-            <Link to='/AdminUser'><li className='a_navli'>Users</li></Link>
-            <Link to='/AdminStudentg'><li className='a_navli'>Student Groups</li></Link>
-            <Link to='/AdminUploads'><li className='a_navli'>Uploads</li></Link>
+            <Link to='/'><li className='a_navli'>Assign panel members</li></Link>
             </ul>
         </nav>
     </div>
--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 //Admin routes
 import { Main } from './Pages/Admin/Main'
+import { Studentg } from './Pages/Admin/Studentg'
+import { Uploads } from './Pages/Admin/Uploads'
+import { Users } from './Pages/Admin/Users'
 
 const App = () =>{
     return(
         <BrowserRouter>
             <Switch>
                 //Admin routes
-                <Route exact path='/' component={Main}/>
+                <Route exact path='/AdminHome' component={Main}/>
+                <Route exact path='/AdminUser' component={Users}/>
+                <Route exact path='/AdminStudentg' component={Studentg}/>
+                <Route exact path='/AdminUploads' component={Uploads}/>
+                
             </Switch>
         </BrowserRouter>
     )
