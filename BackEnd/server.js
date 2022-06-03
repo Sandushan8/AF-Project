dcdc90e63e1355a2ac21db5ad0baa93cb69a8147
Ihill Dewpura <88815545+Sandushan8@users.noreply.github.com>
2022-06-03 10:09:58 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,20 +2,27 @@ const express = require("express");
 const mogoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dotenv = require("dotenv");
+
 require("dotenv").config();
+
+//Ihill files
+const iroutes = require('./Routes/Admin/Adminfunctions')
+
 const app = express();
+const upload = require("./middleware/upload")
 
 //port Number Assign
 const port = process.env.port || 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
 //Database Connection URL
 const URL = process.env.DB_URL;
 
 mogoose.connect(URL,{
+    useNewUrlParser: true,
     useUnifiedTopology:true,
 });
 
@@ -24,6 +31,11 @@ const connection = mogoose.connection;
 connection.once("open",()=>{
     console.log("This database is Connection success!");
 })
+
+
+ app.use("/details",require("./Routes/Student/apiRoutes"));
+ app.use("/topic",upload.single('avatar'),require("./Routes/Student/topicReg"));
+ 
 
 //Thivanka Routes
 app.use("/details",require("./Routes/Student/apiRoutes"));
