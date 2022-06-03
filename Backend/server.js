b5383e3f446026d5a42e3baf4f43c1e39429960d
Ihill Dewpura <88815545+Sandushan8@users.noreply.github.com>
2022-06-03 10:08:52 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,20 +2,27 @@ const express = require("express");
 const mogoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dotenv = require("dotenv");
+
 require("dotenv").config();
+
+//Ihill files
+const iroutes = require('./Routes/Admin/Adminfunctions')
+
 const app = express();
+const upload = require("./middleware/upload")
 
 //port Number Assign
 const port = process.env.port || 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
 //Database Connection URL
 const URL = process.env.DB_URL;
 
 mogoose.connect(URL,{
+    useNewUrlParser: true,
     useUnifiedTopology:true,
 });
 
@@ -25,15 +32,10 @@ connection.once("open",()=>{
     console.log("This database is Connection success!");
 })
 
-//Thivanka Routes
-app.use("/details",require("./Routes/Student/apiRoutes"));
 
-//Punsisi Supervisor-Routes
-const supervisorRouter = require('./Routes/Supervisor/apiRoutes.js');
-
-app.use("/details",require("./Routes/Student/apiRoutes"));
-app.use("/topic",upload.single('avatar'),require("./Routes/Student/topicReg"));
-
+ app.use("/details",require("./Routes/Student/apiRoutes"));
+ app.use("/topic",upload.single('avatar'),require("./Routes/Student/topicReg"));
+ 
 
 //Thivanka Routes
 app.use("/details",require("./Routes/Student/apiRoutes"));
@@ -64,5 +66,5 @@ app.use('/users',require('./Routes/Admin/Users'))
 
 //Display the working port
 app.listen(port,()=>{
-   console.log(`This Server is running in this ${port} port`)
+    console.log(`This Server is running in this ${port} port`)
 })
