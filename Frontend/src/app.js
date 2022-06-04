6b82662f8167a01bbf5e69de2a3efd525d5bfa2e
Ihill Dewpura <88815545+Sandushan8@users.noreply.github.com>
2022-06-04 16:33:54 +0530

    routes

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -83,7 +83,7 @@ const App = () => {
 
                 {/* Student routes */}
 
-                <Route path="/" exact component={LogOutHome} />
+                <Route path="/studentmain" exact component={LogOutHome} />
                 <Route path="/signin" exact component={LoginPage} />
                 <Route path="/signup" exact component={Registration} />
                 <Route path="/home" exact component={HomePage} />
